b8d8e6766d7a40f95f7a4ea3b45c1b4ada50406b
gilhooley <katherine.gilhooley@hackreactor.com>
2014-09-22 14:51:58 -0700

    added base functions

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,30 @@
 // YOUR CODE HERE:
+//
+//
+var _message = {
+  'username': 'teamFetch',
+  'text': 'doop doop',
+  'roomname': 'Interbutts'
+};
+
+var app = {
+
+  init: function() {},
+
+  send: function() {},
+
+  fetch: function() {},
+
+  addMessage: function() {},
+
+  addRoom: function() {},
+
+  handleSubmit: function() {},
+
+  clearMessages: function() {},
+
+  addFriend: function() {}
+
+
+};
 
